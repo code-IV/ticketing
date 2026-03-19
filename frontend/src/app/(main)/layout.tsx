a8fe6803d754024172c5f8b50a0b79abf31eec9a
Mohammed Elias <mamielias186@gmail.com>
2026-03-19 12:10:19 +0300

    profile page redesign and fix user profile update

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -5,6 +5,7 @@ const nav = [
   { name: "games", href: "/games", iconName: "LayoutDashboard" },
   { name: "buy Tickets", href: "/buy", iconName: "Gamepad2" },
   { name: "my bookings", href: "/my-bookings", iconName: "Calendar" },
+  { name: "profile", href: "/profile", iconName: "User" },
   { name: "admin", href: "/admin", iconName: "Users" },
 ];
 
