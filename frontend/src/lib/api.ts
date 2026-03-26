f277882746d6e42c60e623126b3e181463dee23b
Mohammed Elias <mamielias186@gmail.com>
2026-03-26 23:47:27 +0300

    backup api incase .env is not setup

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { logger } from "@/utils/logger";
 
 const API_BASE_URL =
-  process.env.BACKEND_URL ;
+  process.env.BACKEND_URL || "http://localhost:5000/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
